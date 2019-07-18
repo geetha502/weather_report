bb14dbc4296d2c7a0281d997e16febb8673aa657
geetha <geethaa502@gmail.com>
2019-07-18 16:31:33 +0530

    live temperature with proper display

--- temperature.js
+++ temperature.js
@@ -2,13 +2,15 @@
 
 var button= $("#click1");
 var inputText= $("#superinput");
+var output= $("#output");
 
 button.on('click', function(){
   console.log("clicked");
   var cityName= inputText.val();
   var apiUrl= 'http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&units=metric&appid=f8687cf28c78e651500c99fd94acb344';
   $.ajax({url:apiUrl,success:function(result){
-    console.log(result);
+    console.log(result.main.temp);
+    output.html(result.main.temp + "&#8451;");
   }});
 
 
